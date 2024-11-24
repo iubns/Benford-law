bf01170a22f2482f4072695c402f9311d93b4f35
iubns <iubns@naver.com>
2024-11-25 00:03:36 +0900

    update: 데이터 파일 js -> json 으로 변환

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
-import Graph from './component/graph'
-import NumberList from './component/numberList'
-import SelectData from './component/selectData';
-import './App.css';
-import PercentageByNumber from './component/percentageByNumber';
+import { useEffect, useState } from "react"
+import Graph from "./component/graph"
+import NumberList from "./component/numberList"
+import SelectData from "./component/selectData"
+import "./App.css"
+import PercentageByNumber from "./component/percentageByNumber"
 
 function App() {
   const [dataList, setData] = useState([0])
@@ -12,51 +12,43 @@ function App() {
 
   const [renderCount, setRenderCount] = useState(0)
 
-  function renderAndCalc(){
+  function renderAndCalc() {
     const numberCount = new Array(9).fill(0)
-    const processedData = dataList.map(e => {
+    const processedData = dataList.map((e) => {
       let target = e
-      while(target >= 10){
-        target = Number.parseInt(target / 10);
+      while (target >= 10) {
+        target = Number.parseInt(target / 10)
       }
       numberCount[target - 1]++
       return target
     })
     setProcessedData(processedData)
-    setFilterList(numberCount.map(count => (count / dataList.length * 100)))
+    setFilterList(numberCount.map((count) => (count / dataList.length) * 100))
   }
 
-  function SetDataAndRender(dataList){
+  function SetDataAndRender(dataList) {
     setData(dataList)
     setRenderCount(renderCount + 1)
   }
-  
+
   useEffect(renderAndCalc, [renderCount])
 
   return (
     <div className="App">
-      <SelectData setData={SetDataAndRender}/>
-      <div className='result-area'>
-        <Graph
-          className='each-area'
-          calcList={filterList}
-        />
-        <PercentageByNumber
-          className='each-area'
-          filterList={filterList}
-        />
-        {
-          /* 과연 꼭 필요한가?...
+      <SelectData setData={SetDataAndRender} />
+      <div className="result-area">
+        <Graph className="each-area" calcList={filterList} />
+        <PercentageByNumber className="each-area" filterList={filterList} />
+        {/* 과연 꼭 필요한가?...
           <NumberList
             className='each-area'
             originalNumberList={dataList}
             calcList={processedData}
           />
-        */
-        }
+        */}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/component/selectData/index.js
+++ src/component/selectData/index.js
@@ -1,144 +1,151 @@
-import { useEffect, useState } from 'react'
-import deagu from '../../data/covid/deagu'
-import seongnam from '../../data/localTax/seongnam'
-import suwon from '../../data/localTax/suwon'
-import Incheon from '../../data/covid/incheon'
-import sejong from '../../data/localTax/sejong'
-import seoul08 from '../../data/fineDust/seoul08'
-import seoul12 from '../../data/fineDust/seoul12'
-import seoul16 from '../../data/fineDust/seoul16'
-import seoul20 from '../../data/fineDust/seoul20'
-import './index.css'
+import { useEffect, useState } from "react"
+import deagu from "../../data/covid/deagu.json"
+import seongnam from "../../data/localTax/seongnam.json"
+import suwon from "../../data/localTax/suwon.json"
+import Incheon from "../../data/covid/incheon.json"
+import sejong from "../../data/localTax/sejong.json"
+import seoul08 from "../../data/fineDust/seoul08.json"
+import seoul12 from "../../data/fineDust/seoul12.json"
+import seoul16 from "../../data/fineDust/seoul16.json"
+import seoul20 from "../../data/fineDust/seoul20.json"
+import "./index.css"
 
-function SelectData (props) {
-    const [selectedData, setSelectedData] = useState(0)
-    const [selectMenu, setSelectedMenu] = useState(0)
+function SelectData(props) {
+  const [selectedData, setSelectedData] = useState(0)
+  const [selectMenu, setSelectedMenu] = useState(0)
 
-    const dataList = [
+  const dataList = [
+    {
+      title: "무작위 값",
+      menu: [
         {
-            title: '무작위 값',
-            menu: [
-                {
-                    title: '10개',
-                    data: new Array(10).fill(0).map(() => Math.floor(Math.random() * 100) + 1)
-                },
-                {
-                    title: '100개',
-                    data: new Array(100).fill(0).map(() => Math.floor(Math.random() * 100) + 1)
-                },
-                {
-                    title: '1,000개',
-                    data: new Array(1000).fill(0).map(() => Math.floor(Math.random() * 100) + 1)
-                },
-                {
-                    title: '10,000개',
-                    data: new Array(10000).fill(0).map(() => Math.floor(Math.random() * 100) + 1)
-                },
-                {
-                    title: '100,000개',
-                    data: new Array(100000).fill(0).map(() => Math.floor(Math.random() * 100) + 1)
-                },
-            ]
+          title: "10개",
+          data: new Array(10)
+            .fill(0)
+            .map(() => Math.floor(Math.random() * 100) + 1),
         },
         {
-            title: '코로나 바이러스 감염자 수',
-            menu: [
-                {
-                    title: '대구',
-                    data: deagu.filter(value => value)
-                },
-                {
-                    title: '인천 부평구',
-                    data: Incheon.filter(value => value)
-                },
-            ]
+          title: "100개",
+          data: new Array(100)
+            .fill(0)
+            .map(() => Math.floor(Math.random() * 100) + 1),
         },
         {
-            title: '지방세 미환급',
-            menu: [
-                {
-                    title: '성남시',
-                    data: seongnam
-                },
-                {
-                    title: '수원시',
-                    data: suwon
-                },
-                {
-                    title: '세종시',
-                    data: sejong
-                }
-            ]
-        },{
-            title: '미세먼지',
-            menu: [
-                {
-                    title: '서울(08-11)',
-                    data: seoul08
-                },
-                {
-                    title: '서울(12-15)',
-                    data: seoul12
-                },
-                {
-                    title: '서울(16-19)',
-                    data: seoul16
-                },
-                {
-                    title: '서울(20-21)',
-                    data: seoul20
-                },
-            ]
-        }
-    ]
+          title: "1,000개",
+          data: new Array(1000)
+            .fill(0)
+            .map(() => Math.floor(Math.random() * 100) + 1),
+        },
+        {
+          title: "10,000개",
+          data: new Array(10000)
+            .fill(0)
+            .map(() => Math.floor(Math.random() * 100) + 1),
+        },
+        {
+          title: "100,000개",
+          data: new Array(100000)
+            .fill(0)
+            .map(() => Math.floor(Math.random() * 100) + 1),
+        },
+      ],
+    },
+    {
+      title: "코로나 바이러스 감염자 수",
+      menu: [
+        {
+          title: "대구",
+          data: deagu.filter((value) => value),
+        },
+        {
+          title: "인천 부평구",
+          data: Incheon.filter((value) => value),
+        },
+      ],
+    },
+    {
+      title: "지방세 미환급",
+      menu: [
+        {
+          title: "성남시",
+          data: seongnam,
+        },
+        {
+          title: "수원시",
+          data: suwon,
+        },
+        {
+          title: "세종시",
+          data: sejong,
+        },
+      ],
+    },
+    {
+      title: "미세먼지",
+      menu: [
+        {
+          title: "서울(08-11)",
+          data: seoul08,
+        },
+        {
+          title: "서울(12-15)",
+          data: seoul12,
+        },
+        {
+          title: "서울(16-19)",
+          data: seoul16,
+        },
+        {
+          title: "서울(20-21)",
+          data: seoul20,
+        },
+      ],
+    },
+  ]
 
-    function setData(num){
-        setSelectedData(num)
-        setSelectedMenu(0)
-        props.setData(dataList[num].menu[0].data)
-    }
+  function setData(num) {
+    setSelectedData(num)
+    setSelectedMenu(0)
+    props.setData(dataList[num].menu[0].data)
+  }
 
-    function setMenu(num){
-        setSelectedMenu(num)
-        props.setData(dataList[selectedData].menu[num].data)
-    }
+  function setMenu(num) {
+    setSelectedMenu(num)
+    props.setData(dataList[selectedData].menu[num].data)
+  }
 
-    useEffect(() => setData(0), [])
+  useEffect(() => setData(0), [])
 
-    return (
-        <div className='root'>
-            <div className='select-button-list'>
-                {dataList.map((data, index) => 
-                    <div 
-                        className={
-                            `select-button 
-                            ${selectedData === index && 'selected-button'}`
-                        }
-                        onClick={() => setData(index)}
-                        key={"s_" + index}
-                    >
-                        <span>{data.title}</span>
-                    </div>
-                )}
-            </div>
-            <div className='select-button-list' style={{marginTop: '30px'}}>
-                {
-                    dataList[selectedData].menu.map((data, index) => 
-                        <div 
-                            className={
-                                `select-button 
-                                ${selectMenu === index && 'selected-button'}`
-                            } 
-                            onClick={() => setMenu(index)}
-                            key={"sk_" + index}
-                        >
-                            <span>{data.title}({data.data.length})</span>
-                        </div>
-                    )
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className="root">
+      <div className="select-button-list">
+        {dataList.map((data, index) => (
+          <div
+            className={`select-button 
+                            ${selectedData === index && "selected-button"}`}
+            onClick={() => setData(index)}
+            key={"s_" + index}
+          >
+            <span>{data.title}</span>
+          </div>
+        ))}
+      </div>
+      <div className="select-button-list" style={{ marginTop: "30px" }}>
+        {dataList[selectedData].menu.map((data, index) => (
+          <div
+            className={`select-button 
+                                ${selectMenu === index && "selected-button"}`}
+            onClick={() => setMenu(index)}
+            key={"sk_" + index}
+          >
+            <span>
+              {data.title}({data.data.length})
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default SelectData
